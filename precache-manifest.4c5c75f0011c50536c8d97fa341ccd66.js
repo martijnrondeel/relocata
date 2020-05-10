989620fce7b4fdf5850b61595a2049b941721a02
Martijn Rondeel <martijn@rondeel.email>
2020-05-10 18:40:34 +0200

    Updates

---
+++ precache-manifest.4c5c75f0011c50536c8d97fa341ccd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e4ef05a1bc935400dd239ac2c51ab5",
+    "url": "/relocata/index.html"
+  },
+  {
+    "revision": "2ebda8d027a7c3c8f068",
+    "url": "/relocata/static/css/main.de75b4b9.chunk.css"
+  },
+  {
+    "revision": "07abab430114c795d372",
+    "url": "/relocata/static/js/2.8326bf1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/relocata/static/js/2.8326bf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebda8d027a7c3c8f068",
+    "url": "/relocata/static/js/main.43806938.chunk.js"
+  },
+  {
+    "revision": "e0f15edc1000c1e299f5",
+    "url": "/relocata/static/js/runtime-main.a574df83.js"
+  }
+]);
